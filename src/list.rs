f7f33ead6c7d9182df21ed0ffeec613099ebc6a6
Jul Lang <jullanggit@proton.me>
2025-02-07 23:55:49 +0100

    use tokio + async channel

--- src/list.rs
+++ src/list.rs
@@ -1,12 +1,13 @@
+use tokio::task::JoinSet;
+
 use crate::{
     config::CONFIG,
     util::{get_hostname, rerun_with_root_args, system_path},
 };
 use std::{
     collections::HashSet,
-    fs::{self},
-    sync::Mutex,
-    thread::{self, available_parallelism},
+    sync::{Arc, Mutex},
+    time::Duration,
 };
 
 /// Prints all symlinks on the system, that are probably made by dots
@@ -15,7 +16,7 @@ pub fn list(rooted: bool) {
         rerun_with_root_args(&["--rooted"]);
     }
 
-    let items = Mutex::new(HashSet::new());
+    let items = Arc::new(Mutex::new(HashSet::new()));
 
     // Create the workque, using a sender/receiver channel
     let (sender, receiver) = flume::unbounded();
@@ -26,38 +27,50 @@ pub fn list(rooted: bool) {
         sender.send(root_path.into()).unwrap();
     }
 
-    // Create a thread scope
-    thread::scope(|scope| {
-        // For each available cpu core
-        for _ in 0..available_parallelism().unwrap().get() {
-            scope.spawn(|| {
-                // While the channel is open, wait for a path
-                while let Ok(path) = receiver.recv() {
-                    // For each DirEntry under the path (ignoring errors using .flatten())
-                    fs::read_dir(path).unwrap().flatten().for_each(|dir_entry| {
-                        let file_type = dir_entry.file_type().unwrap();
-                        if file_type.is_symlink() {
-                            // get the entries target
-                            let target =
-                                fs::read_link(dir_entry.path()).expect("Failed to get target");
-                            // If the target is in the files/ dir...
-                            if let Ok(stripped) = target.strip_prefix(&CONFIG.files_path)
+    tokio::runtime::Builder::new_current_thread()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let mut join_set = JoinSet::new();
+
+            for _ in 0..10 {
+                let receiver = receiver.clone();
+                let sender = sender.clone();
+                let items = items.clone();
+                join_set.spawn(async move {
+                    // While the channel is open, wait for a path
+                    while let Ok(path) = receiver.recv_timeout(Duration::from_millis(5)) {
+                        // For each DirEntry under the path (ignoring errors using .flatten())
+                        let mut read_dir = tokio::fs::read_dir(path).await.unwrap();
+
+                        while let Ok(Some(dir_entry)) = read_dir.next_entry().await {
+                            let file_type = dir_entry.file_type().await.unwrap();
+                            if file_type.is_symlink() {
+                                // get the entries target
+                                let target = tokio::fs::read_link(dir_entry.path())
+                                    .await
+                                    .expect("Failed to get target");
+                                // If the target is in the files/ dir...
+                                if let Ok(stripped) = target.strip_prefix(&CONFIG.files_path)
                                 // ...and was plausibly created by dots...
                                 && system_path(stripped) == dir_entry.path()
-                            {
-                                // ...add the subpath to the items
-                                let mut items = items.lock().expect("Failed to lock items");
-                                items.insert(stripped.to_owned());
+                                {
+                                    // ...add the subpath to the items
+                                    let mut items = items.lock().expect("Failed to lock items");
+                                    items.insert(stripped.to_owned());
+                                }
+                            } else if file_type.is_dir() {
+                                // Add the path to the queue
+                                sender.send(dir_entry.path()).unwrap();
                             }
-                        } else if file_type.is_dir() {
-                            // Add the path to the queue
-                            sender.send(dir_entry.path()).unwrap();
                         }
-                    });
-                }
-            });
-        }
-    });
+                    }
+                });
+            }
+            drop(sender);
+
+            join_set.join_all().await;
+        });
 
     let items = items.lock().expect("Failed to lock items");
     for item in items.iter() {
