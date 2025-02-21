0112db01e8a45e576b32d5211d3c690ac59db308
Jul Lang <jullanggit@proton.me>
2025-02-21 14:06:36 +0100

    list: queue per thread + single pop -> shared queue + 1/2 drain

--- src/list.rs
+++ src/list.rs
@@ -9,7 +9,6 @@ use crate::{
 };
 use std::{
     fs::{self},
-    iter,
     path::{Path, PathBuf},
     sync::{
         Mutex,
@@ -17,52 +16,6 @@ use std::{
     },
     thread,
 };
-
-#[derive(Default)]
-struct PendingPaths {
-    queue: Mutex<Vec<PathBuf>>,
-    /// the len of the queue
-    len: AtomicUsize,
-    /// the amount of threads currently waiting to lock the queue
-    waiting: AtomicUsize,
-}
-impl PendingPaths {
-    /// Push to the queue.
-    /// Note that this may block the current thread
-    #[expect(clippy::expect_used)] // We only panic if another thread already did
-    fn push(&self, value: PathBuf) {
-        self.queue
-            .lock()
-            .expect("No other threads should panic")
-            .push(value);
-        self.len.fetch_add(1, Ordering::AcqRel);
-    }
-    /// Pop from the queue.
-    /// Note that this may block the current thread
-    #[expect(clippy::expect_used)] // We only panic if another thread already did
-    fn pop(&self) -> Option<PathBuf> {
-        self.queue
-            .lock()
-            .expect("No other threads should panic")
-            .pop()
-            .inspect(|_| {
-                // successful pop -> decrement self.len
-                self.len.fetch_sub(1, Ordering::AcqRel);
-            })
-    }
-    fn len(&self) -> usize {
-        self.len.load(Ordering::Acquire)
-    }
-    fn waiting(&self) -> usize {
-        self.waiting.load(Ordering::Acquire)
-    }
-    fn start_waiting(&self) {
-        self.waiting.fetch_add(1, Ordering::AcqRel);
-    }
-    fn stop_waiting(&self) {
-        self.waiting.fetch_sub(1, Ordering::AcqRel);
-    }
-}
 
 /// Prints all symlinks on the system, that are probably made by dots
 #[expect(clippy::unwrap_used)] // Cant really handle errors in worker threads, we'd unwrap them at some point anyways
@@ -79,42 +32,62 @@ pub fn list(rooted: bool, copy: Option<Vec<String>>) -> Result<()> {
     let threads = thread::available_parallelism().map_or(12, Into::into);
 
     // Set up pending paths
-    // Each thread has its own vec, to which it will push new paths
-    // If a thread's own vec is empty, it will try to get items from another thread's vec
-    // We keep an external atomic len for each vec, so the threads dont have to lock the mutex to see if there are any elements
-    // Additionally we keep a waiting field, so that threads can choose the least waited for vec
-    let pending_paths: Vec<_> = iter::repeat_with(PendingPaths::default)
-        .take(threads)
-        .collect();
-    for (index, path) in CONFIG.list_paths.iter().enumerate() {
-        pending_paths[index].push(path.into());
-    }
+    let pending_paths = Mutex::new(
+        CONFIG
+            .list_paths
+            .iter()
+            .map(PathBuf::from)
+            .collect::<Vec<_>>(),
+    );
 
     let pending = AtomicUsize::new(0);
 
-    // The borrow checker wont let us just capture i in 'for _ in ...', so we have to do this
-    let index = AtomicUsize::new(0);
-
     thread::scope(|scope| {
         for _ in 0..threads {
+            #[expect(clippy::integer_division)]
             scope.spawn(|| {
-                let my_index = index.fetch_add(1, Ordering::Relaxed); // We dont care about ordering
+                let mut local_pending_paths: Vec<PathBuf> = Vec::new();
+
+                // Keep this here to avoid reallocating the vec for every path
+                let mut paths_to_push = Vec::new();
 
                 loop {
-                    // Try our own queue
-                    if let Some(path) = pending_paths[my_index]
-                        .pop()
-                        // Or try stealing a path from another thread's queue
-                        .or_else(|| try_steal_path(&pending_paths, my_index))
-                    {
-                        process_path(&pending_paths, &pending, my_index, &path)
+                    // Try the local queue
+                    if let Some(path) = local_pending_paths.pop() {
+                        // Add ourselves to pending
+                        pending.fetch_add(1, Ordering::AcqRel);
+
+                        // Process path
+                        process_path(&mut paths_to_push, &path)
                             .with_context(|| format!("Failed to process path {}", path.display()))
                             .unwrap();
+
+                        if !paths_to_push.is_empty() {
+                            pending_paths.lock().unwrap().append(&mut paths_to_push);
+                        }
+
+                        // Remove ourselves from pending
+                        pending.fetch_sub(1, Ordering::AcqRel);
+
                         continue;
                     }
 
+                    // Try getting a batch of paths from the shared queue
+                    {
+                        let mut pending_paths = pending_paths.lock().unwrap();
+                        if !pending_paths.is_empty() {
+                            let len = pending_paths.len();
+                            let num_take = (len / 2).max(1); // Take at least 1 element
+
+                            // Add `num_take` paths to `local_pending_paths`
+                            pending_paths
+                                .drain(len - num_take..)
+                                .collect_into(&mut local_pending_paths);
+                        }
+                    } // Drop the lock
+
                     // If no work is left, break
-                    if pending.load(Ordering::Acquire) == 0 {
+                    if local_pending_paths.is_empty() && pending.load(Ordering::Acquire) == 0 {
                         break;
                     }
 
@@ -128,62 +101,7 @@ pub fn list(rooted: bool, copy: Option<Vec<String>>) -> Result<()> {
     Ok(())
 }
 
-/// Try to steal a pending path from another thread.
-fn try_steal_path(pending_paths: &[PendingPaths], my_index: usize) -> Option<PathBuf> {
-    let mut candidate: Option<(usize, usize)> = None; // (thread_index, waiting)
-
-    // For all other threads
-    for (index, pending_paths) in pending_paths.iter().enumerate() {
-        // Skip ourselves
-        if index == my_index {
-            continue;
-        }
-
-        // If the other thread's queue has items
-        if pending_paths.len() > 0 {
-            let waiting = pending_paths.waiting();
-
-            let this_candidate = Some((index, waiting));
-
-            // If no one is currently waiting
-            if waiting == 0 {
-                // Immediately choose this thread
-                candidate = this_candidate;
-                break;
-            }
-
-            // Otherwise, choose the candidate with the smallest waiting count
-            candidate = match candidate {
-                None => this_candidate,
-                Some((_, current_waiting)) if waiting < current_waiting => this_candidate,
-                other => other,
-            };
-        }
-    }
-
-    if let Some((other_index, _)) = candidate {
-        let other_pending_paths = &pending_paths[other_index];
-
-        // start waiting -> pop -> stop waiting
-        other_pending_paths.start_waiting();
-        let stolen = other_pending_paths.pop();
-        other_pending_paths.stop_waiting();
-
-        stolen
-    } else {
-        None
-    }
-}
-
-fn process_path(
-    pending_paths: &[PendingPaths],
-    pending: &AtomicUsize,
-    thread_index: usize,
-    path: &Path,
-) -> Result<()> {
-    // Add ourselves to pending
-    pending.fetch_add(1, Ordering::AcqRel);
-
+fn process_path(paths_to_push: &mut Vec<PathBuf>, path: &Path) -> Result<()> {
     if let Ok(read_dir) = fs::read_dir(path) {
         // Ignore errors with .flatten()
         for dir_entry in read_dir.flatten() {
@@ -227,14 +145,11 @@ fn process_path(
                 // Filter out ignored paths
                 if !CONFIG.ignore_paths.contains(&path) {
                     // Recurse into the dir
-                    pending_paths[thread_index].push(path);
+                    paths_to_push.push(path);
                 }
             }
         }
     }
-
-    // Remove ourselves from pending
-    pending.fetch_sub(1, Ordering::AcqRel);
 
     Ok(())
 }
